dd91b1d070ca52f506caeaabd99d00b61280442b
AEC-業務改革室 <75456370+AEC-Gyoumu-Department@users.noreply.github.com>
2024-02-20 19:53:22 +0900

    Update agv_multisensor.py

--- agv_multisensor.py
+++ agv_multisensor.py
@@ -1,3 +1,10 @@
+# Line tracer and motor motion reference is based in the code finded in https://hellobreak.net/raspberry-pi-pico-line-trace-sensor3/
+#Posted by @KeiMameshiba
+#The aruco mark reference, (when it will be implemented) come form openCv for python Docs.
+#The sonar code used by us is based on codes from experiments and products developed internally at the company.
+#Thanks everyone yours contents was of great help.
+#
+
 import RPi.GPIO as GPIO
 from gpiozero import DistanceSensor
 import time
